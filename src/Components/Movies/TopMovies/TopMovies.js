ca6450630e4208bd080c915d7a2ac612762a5a29
Tomolo997 <tomazovsenjak7@gmail.com>
2021-03-04 10:05:24 +0100

    added the axios get request

--- src/Components/Movies/TopMovies/TopMovies.js
+++ src/Components/Movies/TopMovies/TopMovies.js
@@ -1,6 +1,21 @@
 import React, { Component, Fragment } from 'react';
 import classes from './TopMovies.module.css';
+import axios from 'axios';
+import { API_KEY } from '../../../config';
 export class TopMovies extends Component {
+  state = {
+    movies: null,
+  };
+  componentDidMount() {
+    this.getPopularMovies();
+  }
+
+  getPopularMovies = async () => {
+    const data = await axios.get(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+    );
+    this.setState({ movies: data.data });
+  };
   render() {
     return (
       <div className={classes.TopMovies}>
---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'f7ed630d178b32f81323430bf3604fed';
