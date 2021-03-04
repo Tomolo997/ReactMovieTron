7e648bc148eac866decd578643c95a313bcef454
Tomolo997 <tomazovsenjak7@gmail.com>
2021-03-04 10:35:17 +0100

    added the first card for Top trending movies

--- src/Components/Movies/TopMovies/TopMovies.js
+++ src/Components/Movies/TopMovies/TopMovies.js
@@ -5,21 +5,41 @@ import { API_KEY } from '../../../config';
 export class TopMovies extends Component {
   state = {
     movies: null,
+    showMovies: false,
   };
-  componentDidMount() {
-    this.getPopularMovies();
+  async componentDidMount() {
+    const data = await this.getPopularMovies();
+
+    this.setState({ movies: data });
+    this.setState({ showMovies: true });
   }
 
   getPopularMovies = async () => {
-    const data = await axios.get(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-    );
-    this.setState({ movies: data.data });
+    try {
+      const data = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+      );
+      return data.data.results;
+    } catch (error) {
+      console.log(error);
+    }
   };
   render() {
     return (
       <div className={classes.TopMovies}>
         <div className={classes.TopMovies_Heading}>Top trending movies</div>
+        <div className={classes.Cards}>
+          <div className={classes.Card}>
+            <div className={classes.CardImage}>
+              {this.state.showMovies ? (
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${this.state.movies[2].poster_path}`}
+                  alt=""
+                />
+              ) : null}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
