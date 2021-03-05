83442fe9fe754936b76562b5bb20b064c955faae
Tomolo997 <tomazovsenjak7@gmail.com>
2021-03-05 11:06:21 +0100

    added the search container and basic API call

--- src/Containers/Search/Search.js
+++ src/Containers/Search/Search.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Search.module.css';
-const Search = () => {
-  return (
-    <div className={classes.Search}>
-      <input
-        className={classes.InputSearch}
-        type="text"
-        placeholder="Enter Movie"
-      />
-      <button className={classes.SearchButton}>Search</button>
-    </div>
-  );
-};
+import axios from 'axios';
+import { API_KEY } from '../../config';
+class Search extends Component {
+  state = {
+    currentValue: 'aa',
+  };
+
+  componentDidUpdate() {
+    console.log(this.state.currentValue);
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${this.state.currentValue}&language=en-US&page=1&include_adult=false`
+      )
+      .then((res) => console.log(res));
+  }
+
+  currentValueHandler = (e) => {
+    this.setState({ currentValue: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className={classes.Search}>
+        <input
+          className={classes.InputSearch}
+          type="text"
+          placeholder="Enter Movie"
+          onChange={this.currentValueHandler}
+        />
+        <button className={classes.SearchButton}>Search</button>
+      </div>
+    );
+  }
+}
 
 export default Search;
