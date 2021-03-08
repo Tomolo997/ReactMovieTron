4a8f5f8fd784684e6ebcf8db2b2e21b21effa6a2
Tomolo997 <tomazovsenjak7@gmail.com>
2021-03-08 11:41:31 +0100

    added the search poster path funcitonality

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,20 +3,40 @@ import Navbar from '../../Components/Navigation/Navigation';
 import TopMovies from '../../Containers/Movies/TopMovies/TopMovies';
 import TopTvShows from '../../Containers/Movies/TopTvShows/TopTvShows';
 import Footer from '../Footer/Footer';
+import axios from 'axios';
+import { API_KEY } from '../../config';
+
 class Home extends Component {
   state = {
     searching: false,
+    movies: null,
   };
 
-  search = () => {
+  search = (movies) => {
     this.setState({ searching: true });
+    this.setState({ movies: movies });
   };
 
   render() {
+    let movies = null;
+    if (this.state.movies !== null) {
+      movies = this.state.movies.map((el) => {
+        return (
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <img
+              src={`https://image.tmdb.org/t/p/w400${el.poster_path}`}
+              key={el.id}
+              style={{ color: 'white' }}
+            />
+            {el.name}
+          </div>
+        );
+      });
+    }
     return (
       <div>
         <Navbar search={this.search} />
-        {!this.state.searching ? <TopMovies /> : null}
+        {!this.state.searching ? <TopMovies /> : movies}
         {!this.state.searching ? <TopTvShows /> : null}
 
         <Footer />
--- src/Containers/Search/Search.js
+++ src/Containers/Search/Search.js
@@ -4,19 +4,22 @@ import axios from 'axios';
 import { API_KEY } from '../../config';
 class Search extends Component {
   state = {
-    currentValue: 'aa',
+    currentValue: null,
+    movies: null,
   };
 
-  async componentDidUpdate() {
-    const res = await axios.get(
-      `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${this.state.currentValue}&language=en-US&page=1&include_adult=false`
-    );
-    console.log(res);
+  async componentDidUpdate(_, prevState) {
+    if (prevState.movies == this.state.movies) {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${this.state.currentValue}&language=en-US&page=1&include_adult=false`
+      );
+      this.setState({ movies: res.data.results });
+    }
   }
 
   currentValueHandler = (e) => {
     this.setState({ currentValue: e.target.value });
-    this.props.search();
+    this.props.search(this.state.movies);
   };
 
   render() {
