a09e4d36eca5fffc02dea4e657f1d3c7e05b75dd
Tomolo997 <tomazovsenjak7@gmail.com>
2021-03-09 08:20:30 +0100

    added the movies when already at the id

--- src/Containers/MovieDetail/MovieDetails.js
+++ src/Containers/MovieDetail/MovieDetails.js
@@ -2,6 +2,7 @@ import classes from './MovieDetail.module.css';
 import React, { Component } from 'react';
 import NavBar from '../../Components/Navigation/Navigation';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 import { API_KEY } from '../../config';
 import Footer from '../../Components/Footer/Footer';
 export default class MovieDetails extends Component {
@@ -17,6 +18,19 @@ export default class MovieDetails extends Component {
     similar: null,
     posterPath: null,
     runtime: null,
+    searching: false,
+    movies: null,
+  };
+
+  search = (movies) => {
+    this.setState({ searching: true });
+    this.setState({ movies: movies });
+  };
+
+  setSearchingToFalse = () => {
+    if (this.state.searching === true) {
+      this.setState({ searching: false });
+    } else return;
   };
 
   async componentDidUpdate(prevProps, prevState) {
@@ -72,56 +86,95 @@ export default class MovieDetails extends Component {
         }
       });
     }
+    let movies = null;
+    if (this.state.movies !== null) {
+      movies = this.state.movies.map((el) => {
+        if (el.poster_path) {
+          return (
+            <Link
+              key={el.id}
+              to={`/${el.id}`}
+              onClick={() => this.setState({ searching: false })}
+            >
+              <div
+                key={el.id}
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  color: 'white',
+                }}
+              >
+                <img
+                  style={{ width: '200px', height: '300px' }}
+                  src={`https://image.tmdb.org/t/p/w400${el.poster_path}`}
+                  key={el.id}
+                />
+                <div className={classes.ElName}>{el.name}</div>
+              </div>
+            </Link>
+          );
+        } else {
+          return;
+        }
+      });
+    }
+    let movie = (
+      <div className={classes.Container}>
+        <div className={classes.MovieImage}>
+          <img
+            className={classes.Image}
+            src={`https://image.tmdb.org/t/p/w400${this.state.posterPath}`}
+            alt=""
+          />
+        </div>
+        <div className={classes.Title}>
+          <h1 className={classes.H1Heading}>{this.state.title}</h1>
+        </div>
+        <div className={classes.DescriptionDiv}>
+          <p className={classes.DescriptionParagraph}>
+            {this.state.description}
+          </p>
+        </div>
+        <div className={classes.TimeStamp}>
+          <p className={classes.TimeStampParagraph}>
+            <span className={classes.DescriptionSpan}>Runtime: </span>{' '}
+            {this.state.runtime} minutes
+          </p>
+          <p className={classes.TimeStampParagraph}>
+            <span className={classes.DescriptionSpan}>Budget: </span>{' '}
+            {this.numberWithCommas(+this.state.budget)} $
+          </p>
+          <p className={classes.TimeStampParagraph}>
+            <span className={classes.DescriptionSpan}>Genre: </span>
+            {genres}
+          </p>
+          <p className={classes.TimeStampParagraph}>
+            <span className={classes.DescriptionSpan}> Release date: </span>
+            {this.state.dateOfRelease}
+          </p>
+          <p className={classes.TimeStampParagraph}>
+            <span className={classes.DescriptionSpan}>
+              {' '}
+              <a className={classes.homepageLink} href={this.state.homepage}>
+                Homepage
+              </a>{' '}
+            </span>
+          </p>
+        </div>
+        <div className={classes.CastTitle}>
+          <h1 className={classes.H1HeadingCast}>Cast:</h1>
+        </div>
+        <div className={classes.Cast}>{cast}</div>
+      </div>
+    );
     return (
       <>
-        <NavBar />{' '}
-        <div className={classes.Container}>
-          <div className={classes.MovieImage}>
-            <img
-              className={classes.Image}
-              src={`https://image.tmdb.org/t/p/w400${this.state.posterPath}`}
-              alt=""
-            />
-          </div>
-          <div className={classes.Title}>
-            <h1 className={classes.H1Heading}>{this.state.title}</h1>
-          </div>
-          <div className={classes.DescriptionDiv}>
-            <p className={classes.DescriptionParagraph}>
-              {this.state.description}
-            </p>
-          </div>
-          <div className={classes.TimeStamp}>
-            <p className={classes.TimeStampParagraph}>
-              <span className={classes.DescriptionSpan}>Runtime: </span>{' '}
-              {this.state.runtime} minutes
-            </p>
-            <p className={classes.TimeStampParagraph}>
-              <span className={classes.DescriptionSpan}>Budget: </span>{' '}
-              {this.numberWithCommas(+this.state.budget)} $
-            </p>
-            <p className={classes.TimeStampParagraph}>
-              <span className={classes.DescriptionSpan}>Genre: </span>
-              {genres}
-            </p>
-            <p className={classes.TimeStampParagraph}>
-              <span className={classes.DescriptionSpan}> Release date: </span>
-              {this.state.dateOfRelease}
-            </p>
-            <p className={classes.TimeStampParagraph}>
-              <span className={classes.DescriptionSpan}>
-                {' '}
-                <a className={classes.homepageLink} href={this.state.homepage}>
-                  Homepage
-                </a>{' '}
-              </span>
-            </p>
-          </div>
-          <div className={classes.CastTitle}>
-            <h1 className={classes.H1HeadingCast}>Cast:</h1>
-          </div>
-          <div className={classes.Cast}>{cast}</div>
-        </div>
+        <NavBar searching={this.setSearchingToFalse} search={this.search} />{' '}
+        {!this.state.searching ? (
+          movie
+        ) : (
+          <div className={classes.MovieSearch}>{movies}</div>
+        )}
         <Footer />
       </>
     );
